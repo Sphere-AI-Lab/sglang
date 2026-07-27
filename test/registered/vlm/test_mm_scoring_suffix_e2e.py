1ae13a2db63c5cce429ed6a722136e9d890073ef
JohnWang <chenyangjohn@berkeley.edu>
2026-07-27 14:10:37 -0700

    Fix multimodal suffix test CI registration
    (cherry picked from commit 9dd80e5f8fc52e87dd84a43ebc0e125cd4f4c9d8)

--- test/registered/vlm/test_mm_scoring_suffix_e2e.py
+++ test/registered/vlm/test_mm_scoring_suffix_e2e.py
@@ -22,7 +22,7 @@ from sglang.test.test_utils import (
     popen_launch_server,
 )
 
-register_cuda_ci(est_time=180, suite="base-b-test-1-gpu-large")
+register_cuda_ci(est_time=180, stage="base-b", runner_config="1-gpu-large")
 
 
 def _data_uri():
